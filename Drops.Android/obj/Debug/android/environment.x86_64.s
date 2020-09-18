6f3d83af71b0e11f82d334fb4e07e16abc3cc55e
Max_Gabriel_Susman <gabe.susman@gmail.com>
2020-09-18 17:47:47 -0600

    pushing tmp branch to github

--- Drops.Android/obj/Debug/android/environment.x86_64.s
+++ Drops.Android/obj/Debug/android/environment.x86_64.s
@@ -72,7 +72,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",@progbits,1
 	.type	.L.env.str.8, @object
 .L.env.str.8:
-	.asciz	"3cc03563-4644-4211-a7f4-fabe44193c1d"
+	.asciz	"16eff8a4-a383-4c03-a572-32a6170fbe91"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",@progbits,1
 	.type	.L.env.str.9, @object
