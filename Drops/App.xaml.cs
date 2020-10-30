ed16f91eba92ef23a8136335a14d30a8859034fa
Max_Gabriel_Susman <gabe.susman@gmail.com>
2020-10-30 02:52:53 -0600

    added logic for style selection based on platform as well as file tree representation in README

--- Drops/App.xaml.cs
+++ Drops/App.xaml.cs
@@ -9,19 +9,94 @@ namespace Drops
 {
     public partial class App : Application
     {
+        // FIELDS
         const int smallWightResolution = 768;
+
         const int smallHeightResolution = 1280;
+
         // CONSTRUCTORS
+        //public App()
+        //{
+        //    InitializeComponent();
+
+        //    MainPage = new NavigationPage(new LoginPage());
+
+
+        //}
         public App()
         {
             InitializeComponent();
+            LoadStyles();
 
-            MainPage = new NavigationPage(new LoginPage());
-
-
+            MainPage = new MainPage();
         }
 
-       
+        // METHODS
+        void LoadStyles()
+        {
+            // I need to create logic that implements styles dependent upon platform
+            switch (Device.RuntimePlatform)
+            {
+                case Device.iOS:
+
+                    System.Diagnostics.Debug.WriteLine("you're running iOS");
+
+                    // Let's add style selection logic
+
+                    break;
+
+                case Device.Android:
+
+                    System.Diagnostics.Debug.WriteLine("you're running android");
+
+                    // let's add style selection logic
+                    break;
+
+                default:
+
+                    System.Diagnostics.Debug.WriteLine("gee I don't know what you're running, maybe uwp?");
+
+                    break;
+            }
+
+            // This logic will be implemented later for tablet and desktop support
+            if (IsASmallDevice())
+            {
+                dictionary.MergedDictionaries.Add(SmallDevicesStyle.SharedInstance);
+            }
+            else
+            {
+                dictionary.MergedDictionaries.Add(GeneralDevicesStyle.SharedInstance);
+            }
+        }
+
+        // I'm going to implement this in the future to make the code adaptive
+        //switch (Device.RuntimePlatform)
+        //{
+        //    case Device.iOS:
+        //        // dropsglyph.pdf assing this to the 
+        //        break;
+        //    case Device.Android:
+
+        //        break;
+        //    default:
+
+        //        break;
+        //}
+
+        //switch (Device.Idiom)
+        //{
+        //    case TargetIdiom.Desktop:
+
+        //        break;
+        //    case TargetIdiom.Phone:
+        //        break;
+        //    case TargetIdiom.Tablet:
+        //        break;
+        //    case TargetIdiom.TV:
+        //        break;
+        //}
+
 
         public static bool IsASmallDevice()
         {
--- README.md
+++ README.md
@@ -6,9 +6,37 @@
 RealDrops is an outdoor Sales Lead management application that allows users to represent potential leads and other entities with a pin(referred to as a drop) using their devices default maps API. Drops are persisted in collections that are members of area objects that have a pair of coordinates to center the map view upon rendering.
 Areas also have a collection of 'Subscribers' these are users  who are allowed to access the area and manage it's drops, the subscribers include the owner(the creator of the area) and all the users he has shared this area with. Data is persisted by an Azure Cosmos DB backend, tandem local persistence is still in the works so the application currently requires a network connection.
 
+## File Tree
+
+Drops
+
+	Drops
+
+		Helpers : I also want to move this content to static to consolidate ../ members
+
+		Models : Object Models
+
+		Services : I think I'm going to move this Dirs content to Static
+
+		Static : MetaData for Object manipulation and DB interaction
+
+		ViewModels : Value
+
+		Views : ContentPages w/ XAML
+
+		Resources : I haven't made this one yet but I want it to contain all of the Resource files 
+
+	Drops.Android
+
+		...
+
+	Drops.iOS
+
+		...
+
 ## Compatible Environments
 
-* iOS
+- iOS
 
 ## In the Works
 
