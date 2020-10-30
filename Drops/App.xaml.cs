b4542a4291d0d416e62648674443ff2b00afc575
Max_Gabriel_Susman <gabe.susman@gmail.com>
2020-10-29 23:37:20 -0600

    added logic to app.xaml.cs that will allow me to capture metrics for the users display

--- Drops/App.xaml.cs
+++ Drops/App.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Xamarin.Essentials;
 using Drops.Views;
 
 
@@ -8,12 +9,31 @@ namespace Drops
 {
     public partial class App : Application
     {
+        const int smallWightResolution = 768;
+        const int smallHeightResolution = 1280;
         // CONSTRUCTORS
         public App()
         {
             InitializeComponent();
 
             MainPage = new NavigationPage(new LoginPage());
+
+
+        }
+
+       
+
+        public static bool IsASmallDevice()
+        {
+            // Get Metrics
+            var mainDisplayInfo = DeviceDisplay.MainDisplayInfo;
+
+            // Width (in pixels)
+            var width = mainDisplayInfo.Width;
+
+            // Height (in pixels)
+            var height = mainDisplayInfo.Height;
+            return (width <= smallWightResolution && height <= smallHeightResolution);
         }
     }
 }
